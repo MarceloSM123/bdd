5a598121599c4257834cd2265a267be5e6f4af22
123MARCE567lenin <ultradjklenin@hotmail.com>
2025-10-29 22:14:44 -0500

    ingreso de nuevos datos en los scripts

---
+++ nuevas inserciones/cuentas.sql
@@ -0,0 +1,67 @@
+drop table cuentas
+
+create table cuentas(
+	numero_cuenta char(5) not null,
+	cedula_propietario char(5) not null ,
+	fecha_creacion date not null,
+	saldo money not null,
+	constraint cuentas_pk primary key(numero_cuenta)
+)
+
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12345', '45678', '10/01/2022', 22.22)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12355', '45688', '21/02/2020', 10.33)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12365', '45698', '12/03/2021', 5.66)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12375', '45608', '23/03/2022', 2.55)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12385', '45618', '14/04/2023', 9.66)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12395', '45672', '25/05/2024', 7.88)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12305', '45673', '16/06/2025', 8.55)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12341', '45671', '19/07/2020', 25.36)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12342', '45675', '17/08/2021', 78.98)
+insert into cuentas(numero_cuenta, cedula_propietario, fecha_creacion,saldo)
+values ('12343', '45676', '18/09/2022', 45.66)
+
+select * from cuentas
+
+-- selecciones 
+
+select numero_cuenta, saldo from cuentas
+select * from cuentas where fecha_creacion between '29/10/2025' and '29/8/2025'
+select numero_cuenta, saldo from cuentas where fecha_creacion between '29/10/2025' and '29/8/2025'
+
+-- actualizaciones
+update cuentas set saldo=10 where cedula_propietario like '17%'
+
+--eliminar
+delete from cuentas where cedula_propietario like '10%'
+
+-- nuevos datos 
+--CUENTAS
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25569,17504,'15/01/2030',500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25570,17500,'01/11/2028',600);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25571,17501,'22/01/2023',700);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25572,17508,'30/05/2022',800);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25573,17509,'15/03/2023',900);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25574,17510,'15/05/2023',1000);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25575,17507,'15/09/2023',1500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25576,17515,'15/10/2024',2500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25577,17512,'15/08/2025',3500);
+insert into cuentas (numero_cuenta,cedula_propietario,fecha_creacion,saldo)
+values (25578,17503,'15/06/2017',4500);
---
+++ nuevas inserciones/estudiantes.sql
@@ -0,0 +1,64 @@
+drop table estudiantes
+
+create table estudiantes (
+	cedula char(10) not null,
+	nombre varchar(50) not null ,
+	apellido varchar(50) not null,
+	email varchar(50) not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key(cedula)
+)
+
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1234567890','Marcelo', 'Salcedo', 'asd@gmail.com','23/05/1992')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1232567890','Mariela', 'Cruz', 'cd@gmail.com','27/06/2000')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1233567890','Marco', 'Espejo', 'esd@gmail.com','26/07/2021')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1235567890','michele', 'velasquez', 'ved@gmail.com','25/08/1995')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1236567890','Carla', 'Rodriguez', 'rad@gmail.com','24/09/1980')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1237567890','Maria', 'smith', 'sbd@gmail.com','23/10/1992')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1238567890','Diana', 'Salazar', 'sad@gmail.com','22/11/1990')
+insert into estudiantes (cedula, nombre, apellido, email,fecha_nacimiento)
+values ('1234597890','Fernanda', 'Ochoa', 'ocd@gmail.com','12/01/1991')
+
+select * from estudiantes
+
+-- selecciones
+select nombre, cedula from estudiantes
+select nombre from estudiantes where cedula like '17%'
+select nombre from estudiantes where nombre like 'A%'
+
+--actualizaciones 
+
+update estudiantes set apellido='Hernandez' where cedula like '17%'
+
+-- eliminar
+
+delete from estudiantes where cedula like '%05'
+
+--nuevos datos 
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1235476801,'Raul','Martínez','raumart01@gmail.com','04/02/2003');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1735476802,'Mario','Guaman','MarioG25@gmail.com', '08/12/2000');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1235476803,'Roberto','Quishpe','RobQuishpe64@gmail.com','29/06/2005');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1735476804,'Paul','Noguera','PaulNog55@gmail.com','22/07/2002');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1235476805,'Marcelo','Ramos','MarceloR72@gmail.com','15/08/2008');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1735476806,'Anthony','Agual','KAgual22@gmail.com','25/03/2002');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1235476807,'Paula','Celi','PauCeli31@gmail.com','30/09/2010' );
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1735476808,'Mónica','Martínez','MoniMar15@gmail.com','22/01/2001');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1235476809,'Anabel','Perlaza','WPerlaza18@gmail.com','12/04/2000');
+insert into estudiantes (cedula,nombre,apellido,email,fecha_nacimiento)
+values (1735476810,'Sofía','Jimenez','SofiJz22@gmail.com','22/01/2001');
